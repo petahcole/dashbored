9e893aa46429741af3e3309a0d6c583fd2498b55
jordan-storz <jordan.storz.1@gmail.com>
2017-01-03 10:21:08 -0700

    created stub routes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:id', function(){});
+
+router.put('/:id', function(){});
+
+router.post('/', function(){});
+
+router.get('/guest', function(){});
+
+
 module.exports = router;
