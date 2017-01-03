cc93950d2c8e369a33e6fd851fd13b574bf8e653
jordan-storz <jordan.storz.1@gmail.com>
2017-01-03 13:25:11 -0700

    create make-url helper function file

--- api/webhose.js
+++ api/webhose.js
@@ -1,33 +1,14 @@
 const request = require('request');
 const R = require('Ramda');
+const makeUrl = require('./helpers/make-url');
 
 
 module.exports = (function() {
 
   const ENDPOINT = `https://webhose.io/search?token=3cae7ae3-71d5-45cc-a0b1-5ab0fd08da19&language=english`;
 
-
-  const renderQuery = function(obj) {
-    if (!obj) {
-      return '';
-    } else {
-      return '&' + R.toPairs(obj)
-        .map(pair => pair.map(words => words.replace(' ', '%20')))
-        .map(pair => pair.join('='))
-        .join('&');
-    }
-  }
-
-  const makeUrl = function(queryObj) {
-    if (!queryObj) {
-      return ENDPOINT;
-    } else {
-      return ENDPOINT + renderQuery(queryObj);
-    }
-  }
-
   const getNews = function(queryObj) {
-    let url = makeUrl(queryObj);
+    let url = makeUrl(ENDPOINT, queryObj);
     return new Promise(function (resolve, reject) {
       request(url, function(err, res, body) {
         if (!err) {
