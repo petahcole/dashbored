325809e5062d12f75682c1ecab09bbd6df68f663
jordan-storz <jordan.storz.1@gmail.com>
2017-01-04 14:57:31 -0700

    edit

--- public/javascripts/eventful.js
+++ public/javascripts/eventful.js
@@ -1,4 +1,4 @@
-// $(document).ready(function()    {
+$(document).ready(function()    {
 console.log("working!");
     $.get("/templates/events").then((eventInfo) => {
 
@@ -14,4 +14,4 @@ console.log("working!");
 
 
 
-// })
+})
