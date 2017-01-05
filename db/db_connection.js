a7bd222fb5382cba59113efee663cb5158873b92
jordan-storz <jordan.storz.1@gmail.com>
2017-01-05 16:14:25 -0700

    change process

--- db/db_connection.js
+++ db/db_connection.js
@@ -1,3 +1,3 @@
-var environment = process.env.DEV_DATABASE_URL || 'development';
+var environment = process.env.NODE_ENV || 'development';
 var config = require('../knexfile.js')[environment];
 module.exports = require('knex')(config);
