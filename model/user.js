6c8feea628b9f23b7b711b6c3b61a999bbb49db4
jordan-storz <jordan.storz.1@gmail.com>
2017-01-05 16:00:44 -0700

    fix syntax

--- model/user.js
+++ model/user.js
@@ -1,27 +1,27 @@
+
+var knex = require('../db/db_connection')
+
 module.exports = {
-        var knex = require('../db/db_connection')
 
-        module.exports = {
+    loadDash: function(username) {
+        return knex('user')
+            .join('user_preference', 'user_preference.user_id', '=', 'user.id')
+            .join('user_role', 'user_role.user_id', '=', 'user.id')
+            .select('user.username', 'user.password', 'user_preference.pref_type_id', 'user_role.user_id')
+            .where('user.username', username)
 
-            loadDash: function(username) {
-                return knex('user')
-                    .join('user_preference', 'user_preference.user_id', '=', 'user.id')
-                    .join('user_role', 'user_role.user_id', '=', 'user.id')
-                    .select('user.username', 'user.password', 'user_preference.pref_type_id', 'user_role.user_id')
-                    .where('user.username', username)
-
-            },
-            validSignUp: function(username) {
-                return knex('user').where('username', username)
-            },
-            validSignIn: function(username, password) {
-                return knex('user').where({
-                    username: username,
-                })
-            },
-            createUser: function(userInfo) {
-                return knex('user').returning('id').insert(userInfo)
-            }
+    },
+    validSignUp: function(username) {
+        return knex('user').where('username', username)
+    },
+    validSignIn: function(username, password) {
+        return knex('user').where({
+            username: username,
+        })
+    },
+    createUser: function(userInfo) {
+        return knex('user').returning('id').insert(userInfo)
+    }
 
 
-        }
+}
