03beb023a38ce2056875c09511afd9534286bb15
Michael Roque <mikeroque1@gmail.com>
2017-01-05 13:54:50 -0700

    more login stuff

--- model/user.js
+++ model/user.js
@@ -1,4 +1,4 @@
-// knex
+
 var knex = require('../db/db_connection')
 
 module.exports = {
@@ -20,14 +20,3 @@ module.exports = {
         })
     }
   }
-
-//
-//
-//     ```sql
-//     SELECT u.username, u.password, up.pref_type_id, ur.role_id
-//     FROM "user" u
-//     JOIN user_preference up ON up.user_id = u.id
-//     JOIN user_role ur ON ur.user_id = u.id
-//     WHERE u.id = up.user_id
-//     AND ur.user_id = u.id;
-//     ```
