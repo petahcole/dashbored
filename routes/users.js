e50be90363903027eff3bed11c012c0502e3e4c9
cunnellp5 <philip.cunnell@colorado.edu>
2017-01-06 15:20:37 -0700

    SWERVICE SIDEE VALIDATORR

--- routes/users.js
+++ routes/users.js
@@ -38,53 +38,33 @@ router.get('/:id', function(req, res, next) {
 
 router.put('/:id', function(req, res, next) {});
 
-
-function validUser(user){
-  const validUserName = typeof user.username == 'string' &&
-                      user.username.trim() != '';
-  const validEmail = typeof user.email == 'string' &&
-                      user.email.trim() != '';
-  const validPassword = typeof user.password == 'string' &&
-                      user.password.trim() != '' &&
-                      user.password.trim().length >= 5;
-  return validEmail && validPassword && validUserName
-}
-
 router.post('/', function(req, res, next) {
-    var userInfo = {
-        username: req.body.username,
-        password: bcrypt.hashSync(req.body.password),
-        email: req.body.email,
-        joined: new Date()
-    }
-
-    if (validUser(req.body)) {
-      user.validSignUp(req.body.email)
-        .then(result => {
-          if (result.length != 0) {
-            // this is unique
-            //then hash password
-            bcrypt.hash(req.body.password, 10)
-              .then((hash) => {
-                //store hash in passwrod db
-                var userPrefIds = extractPrefIds(req.body)
-                user.createUser(userInfo).then(function(result) {
-                  var userId = result[0];
-                  userPref.savePreferences(userId, userPrefIds).then(function(data) {
-                    setCookie(res, {dashId: userId}).then(() => {
-                      res.redirect(`/users/${userId}`);
-                    });
-                  })
-                })
-              })
-          } else {
-            next(new Error('email or username already in use'))
-          }
-        })
-    } else {
-      next(new Error('Invalid user'))
-    }
-});
+  var userInfo = {
+         username: req.body.username,
+         password: bcrypt.hashSync(req.body.password),
+         email: req.body.email,
+         joined: new Date()
+     }
+  console.log("hit me");
+     user.createUser(userInfo).then(function(result) {
+     var userId = result[0];
+     userPref.savePreferences(userId, userPrefIds).then(function(data) {
+       setCookie(res, {dashId: userId}).then(() => {
+         res.redirect(`/users/${userId}`);
+       });
+     })
+   }).catch(function(err){
+     if (err.constraint === "user_username_unique") {
+       res.send({
+         message: 'User name taken already!'
+       })
+     } else if (err.constraint == "user_email_unique"){
+       res.send({
+         message: 'Email already taken!'
+       })
+     }
+   })
+})
 
 
 function extractPrefIds(obj) {
